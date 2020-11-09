913fa97ea2b96a620851dc9bb21dbfd0ee2c2c5c
Rafael Guedes <r.guedes@oceanum.science>
2020-11-09 21:30:18 +1300

    Fix package installation

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     include_package_data=True,
     keywords="stats",
     name="stats",
-    packages=find_packages(include=["stats"]),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
