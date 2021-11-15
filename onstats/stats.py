cc023603c42b5e85df7054ce8e1a598f2cd41134
Rafael Guedes <r.guedes@oceanum.science>
2021-11-15 18:19:00 +1300

    Replace Stats class by the new version

--- onstats/stats.py
+++ onstats/stats.py
@@ -1,5 +1,6 @@
 """Calculated gridded stats using xarray and dask."""
 import os
+from pathlib import Path
 import shutil
 import yaml
 import logging
@@ -9,6 +10,7 @@ import pandas as pd
 import xarray as xr
 import dask.array as da
 from fsspec import get_mapper
+from intake import open_catalog
 
 import dask
 from dask.diagnostics import ProgressBar
@@ -180,7 +182,7 @@ class DerivedVar:
         )
 
 
-class Stats(DerivedVar):
+class StatsDeprecated(DerivedVar):
     def __init__(
         self,
         dataset,
@@ -1306,6 +1308,296 @@ class Stats(DerivedVar):
             encoding[data_var].update(self.dsout[data_var].encoding)
         # Loading into memory before saving to disk. We may want to reassess this.
         self._load()
+        self._sortby()
+        self._setattrs()
+        self.dsout.to_netcdf(outfile, format=format, encoding=encoding)
+        if self.updir:
+            self._upload(outfile)
+
+    def to_zarr(
+        self,
+        outfile,
+        dsout=None,
+        _FillValue=-32767,
+        mode="w",
+        chunksizes={"": {}},
+        **kwargs,
+    ):
+        """Save output dataset as zarr.
+
+        Args:
+            outfile (str): Base name of output zarr file.
+            dsout (str): Output dataset to write, self.dsout by default.
+            _FillValue (int): Fill Value.
+            mode (str): Zarr write mode.
+            chunksizes (dict): Key is a suffix for outfile, values are chunks, one file
+                is saved for each key-value in the dictionary.
+
+        """
+        logger.debug(f"Saving stats dataset into file: {outfile}")
+        dsout = dsout or self.dsout
+        for data_var in dsout.data_vars:
+            dsout[data_var].encoding.update({"_FillValue": _FillValue})
+            dsout[data_var].encoding.pop("zlib", None)
+        self._sortby()
+        self._setattrs()
+
+        basename, ext = os.path.splitext(outfile)
+        for suffix, chunks in chunksizes.items():
+            if suffix and not suffix.startswith("_"):
+                suffix = "_" + suffix
+            store = f"{basename}{suffix}{ext}"
+            logger.info(f"Writing zarr file {store} with chunks {chunks}")
+            fsmap = get_mapper(store)
+            included_chunks = {c: v for c, v in chunks.items() if c in dsout.dims}
+            dsout.chunk(included_chunks).to_zarr(fsmap, consolidated=True, mode=mode)
+            if self.updir:
+                self._upload(store)
+
+
+
+class Stats:
+    def __init__(
+        self,
+        outfile,
+        dset=None,
+        urlpath=None,
+        engine="zarr",
+        catalog=None,
+        dataset_id=None,
+        mapping={},
+        slice_dict={},
+        updir=None,
+        localdir="/scratch",
+        allow_split_large_chunks=False,
+        n_workers=None,
+        calls=[],
+        **kwargs,
+    ):
+        """Gridded stats using dask arrays.
+
+        Args:
+            outfile (str): Name or URI of output file, must end with ".nc" or ".zarr".
+            dset (xr.Dataset): Source dataset to calculate stats from.
+            urlpath (str): Path or URI of dataset file to calculate stats from.
+            engine (str): Engine to use with xr.open_dataset when using urlpath.
+            catalog (str): Path or URI of intake catalog with datasets to open.
+            dataset_id (str): Intake dataset id to calculate stats from.
+            mapping (dict): Dictionary for renaming dataset variables.
+            slice_dict (dict): Dictionary specifying slicing arg.
+            updir (str): Upload direction to upload netcdf and zarr stats files to.
+            allow_split_large_chunks (bool): Allow dask auto-resize of small chunks.
+            n_workers (int): Number of workers for local dask distributed cluster.
+            calls (list): List of dicts defining each stats method to run with keys:
+                method: name of stats method to run.
+                kwargs: kwargs to run the stats method.
+
+        Note:
+            You must provide one of 'dset', 'urlpath' or ['catalog', 'dataset_id'].
+
+        """
+        dask.config.set({"array.slicing.split_large_chunks": allow_split_large_chunks})
+
+        if not any([dset, urlpath, catalog and dataset_id]):
+            raise ValueError(
+                "You must provide one of dset, urlpath or [catalog, dataset_id]"
+            )
+
+        self.outfile = outfile
+        self.dset = dset
+        self.urlpath = urlpath
+        self.catalog = catalog
+        self.dataset_id = dataset_id
+        self.engine = engine
+        self.mapping = mapping
+        self.slice_dict = slice_dict
+        self.n_workers = n_workers
+        self.updir = updir
+        self.calls = calls
+
+        self.dsout = xr.Dataset()
+
+    def __call__(self):
+        """Loop over list of dictionary to execute stats methods.
+
+        Local dask clusters are set up for running each stats method.
+
+        """
+        # Execute each stats method
+        for call in self.calls:
+            method = call["method"]
+            kwargs = call.get("kwargs", {})
+            logger.info(f"Stat.{method}({kwargs})")
+            with Client(processes=True, n_workers=self.n_workers) as client:
+                logger.info(client)
+                getattr(self, method)(**kwargs)
+            self._clean_dask_worker_space()
+
+        # Save output file
+        if self.outfile.endswith(".nc"):
+            self.to_netcdf(self.outfile)
+        elif self.outfile.endswith(".zarr"):
+            self.to_zarr(self.outfile)
+
+    def _clean_dask_worker_space(self):
+        """Remove existing dask worker space directory."""
+        p = Path("./dask-worker-space")
+        if p.is_dir():
+            logger.info("Removing existing dask worker space")
+            shutil.rmtree(p)
+
+    def _open_dataset(self, chunks={}):
+        """Open and slice dataset according to the init attributes provided.
+
+        Args:
+            chunks (dict): Mapping dim: size for chunking each dimension, 
+                only used if opening dataset from urlpath to avoid rechunking.
+
+        """
+        logger.info("Open dataset")
+
+        # Open dataset
+        if isinstance(self.dset, xr.Dataset):
+            logger.debug("Dataset provided")
+            dset = self.dset
+        elif isinstance(self.dset, str):
+            raise ValueError(
+                f"dset must be a dataset instance, got {self.dset}. Use 'urlpath' "
+                "and 'engine' to specify a dataset path to open"
+            )
+        elif self.urlpath:
+            logger.debug(f"Open dataset from urlpath: {self.urlpath}")
+            dset = xr.open_dataset(self.urlpath, engine=self.engine, chunks=chunks)
+        elif self.catalog and self.dataset_id:
+            logger.debug(f"Open dataset from intake: {self.catalog}-{self.dataset_id}")
+            cat = open_catalog(self.catalog)
+            if not cat:
+                raise ValueError(f"Cannot open intake catalog from {self.catalog}")
+            dset = cat[self.dataset_id].to_dask()
+        else:
+            raise ValueError("Cannot identify source dataset from input arguments")
+
+        # Rename dataset
+        mapping = {k: v for k, v in self.mapping.items() if k in dset}
+        if mapping:
+            logger.debug(f"Rename {dset} as {mapping}")
+            dset = dset.rename(mapping)
+
+        # Slice dataset
+        dset = self._slice(dset)
+
+        logger.info(f"Source dataset:\n{dset}")
+        return dset
+
+    def _slice(self, dset):
+        """Slice dataset from slice_dict attribute."""
+        for slice_method, slice_kwargs in self.slice_dict.items():
+            dset = getattr(dset, slice_method)(**slice_kwargs)
+            sizes = {k: v.size for k, v in ds.coords.items()}
+            for k, v in sizes.items():
+                if v == 0:
+                    raise ValueError(
+                        f"Slicing dataset {dset} with slice_dict {self.slice_dict}"
+                        f" resulted in coordinate {k} with zero size"
+                    )
+        return dset
+
+    def _upload(self, filename):
+        """Upload stats files.
+
+        Args:
+            filename (str): Name of file to upload.
+
+        """
+        outfile = os.path.join(self.updir, os.path.basename(filename))
+        logger.info(f"Uploading {filename} --> {outfile}")
+        if exists(outfile):
+            logger.debug(f"Removing existing file {outfile} before uploading")
+            rm(outfile, recursive=isdir(outfile))
+        put(filename, outfile, recursive=isdir(filename))
+
+    def _sortby(self):
+        """Sort output dataset by all coordinates."""
+        for name, coords in self.dsout.coords.items():
+            if coords[0] > coords[-1]:
+                logger.info(f"Sorting by coordinate {name}")
+                self.dsout = self.dsout.sortby(name)
+
+    def _setattrs(self):
+        """Define some attributes in output dataset."""
+        if "quantile" in self.dsout.coords:
+            self.dsout["quantile"].attrs = {
+                "standard_name": "quantile",
+                "long_name": "quantile",
+                "units": "",
+            }
+
+    def apply_func(
+        self,
+        func,
+        dim="time",
+        data_vars=[],
+        group=None,
+        suffix=None,
+        compute=True,
+        chunks={},
+        **kwargs,
+    ):
+        """apply xarray function.
+
+        Args:
+            func (str): Name of valid xarray function to apply.
+            dim (str): Dimension to apply function over.
+            data_vars (list): Data vars to apply stats over, "all" for all variables.
+            group (str): Time grouping type, any valid time_{group} such month, season.
+            suffix (str): String to append to each variable name in output dataset,
+                defined as `f"_{func}"` if `suffix==None`.
+            compute (bool): Compute dask variables from output dataset before returning.
+            kwargs: kwargs for function func.
+
+        """
+        if suffix is None:
+            suffix = f"_{func}"
+
+        # Open dataset
+        dset = self._open_dataset(chunks=chunks)
+
+        # Selecting variables
+        if data_vars == "all":
+            data_vars = [k for k in dset.data_vars]
+        logger.debug(f"Calculating {dim}-{func} for vars: {data_vars}")
+        dset = dset[data_vars]
+
+        # Grouping by
+        if group:
+            logger.info(f"Grouping by {group}")
+            suffix += f"_{group}"
+            dset = dset.groupby(f"time.{group}")
+
+        # Calculate stats
+        dsout = getattr(dset, func)(dim=dim, **kwargs)
+        if compute:
+            dsout = dsout.load()
+
+        # Rename
+        dsout = dsout.rename({v: f"{v}{suffix}" for v in dsout.data_vars.keys()})
+
+        # Merge onto output
+        self.dsout = self.dsout.merge(dsout)
+
+        return dsout
+
+    def to_netcdf(self, outfile, format="NETCDF4", _FillValue=-32767):
+        """Save output dataset as netcdf.
+
+        Args:
+            outfile (str): Name of output netcdf file.
+            format (str): Output Netcdf file format.
+            _FillValue (int): Fill Value.
+
+        """
+        logger.debug(f"Saving stats dataset into file: {outfile}")
+        encoding = {v: {"zlib": True} for v in self.dsout.data_vars}
         self._sortby()
         self._setattrs()
         self.dsout.to_netcdf(outfile, format=format, encoding=encoding)
