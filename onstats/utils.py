243a1d3255ca28fd3788d8800a3a55620633cefd
Rafael Guedes <r.guedes@oceanum.science>
2021-11-23 11:49:37 +1300

    Add decorator to calculate stepwise functions

--- onstats/utils.py
+++ onstats/utils.py
@@ -1,10 +1,104 @@
 """Auxiliary functions."""
+import logging
 import numpy as np
 import dask.array as da
 import pandas as pd
 import xarray as xr
 from scipy import stats, signal
 from subprocess import run, PIPE, STDOUT
+from inspect import signature, Parameter
+from functools import wraps
+
+
+logger = logging.getLogger(__name__)
+
+
+def _get_default_parameters(func):
+    """Default kwargs from function."""
+    param = signature(func).parameters
+    kw = {k: v.default for k, v in param.items() if v.default is not Parameter.empty}
+    return kw
+
+
+def stepwise(func):
+    """Execute func on loaded slices of dataset in a stepswise manner.
+
+    This decorator is intended to run functions requiring single time chunks (e.g. rpv,
+        quantile) onto large datasets that are not chunked with single time chunks and
+        require rechunking which can require prohibitively large amounts of RAM.
+
+    Required signature in decorated function func:
+        - self as the first arg as function is attached to Stats class dynamically.
+        - dset as either a kwarg or the second arg.
+        - yname, xname specifying dimension names to stepwise through.
+        - ystep, xstep specifying dimension sizes to stepwise through.
+
+    """
+    @wraps(func)
+    def wrapped_func(*args, **kwargs):
+
+        # Stepwise applied only if both steps are provided
+        if kwargs.get("ystep", None) is None and kwargs.get("xstep", None) is None:
+            logger.info("No stepwise applied")
+            return func(*args, **kwargs)
+
+        # Default kwargs that may have not been set
+        kwdef = _get_default_parameters(func)
+
+        # Ensure function has required kwargs
+        for key in ["yname", "xname", "ystep", "xstep"]:
+            if key not in kwargs and key not in kwdef:
+                raise ValueError(
+                    f"stepwise decorator requires '{key}' kwarg in func but the "
+                    f"func signature is: {func.__name__}{signature(func)}"
+                )
+
+        # Dataset
+        dset = kwargs.pop("dset", args[1])
+        if not isinstance(dset, xr.Dataset):
+            ValueError("stepwise decorator requires dset as a kwarg or the first arg")
+
+        # Coords names
+        yname = kwargs.pop("yname", kwdef["yname"])
+        xname = kwargs.pop("xname", kwdef["xname"])
+        for name in [yname, xname]:
+            if name not in dset.dims:
+                raise ValueError(f"'{name}' is not a dimension of dataset {dset.dims}")
+
+        # Dims and steps sizes
+        yend = dset[yname].size
+        xend = dset[xname].size
+        ystep = kwargs.pop("ystep", kwdef["ystep"]) or yend
+        xstep = kwargs.pop("xstep", kwdef["xstep"]) or xend
+        if yend % ystep != 0:
+            yend += ystep
+        if xend % xstep != 0:
+            xend += xstep
+
+        # Steps intervals
+        y_intervals = pd.interval_range(start=0, end=yend, freq=ystep)
+        x_intervals = pd.interval_range(start=0, end=xend, freq=xstep)
+        n_intervals = len(y_intervals) * len(x_intervals)
+
+        # Compute each spatial box slice loading before calculating stats
+        i = 1
+        dsout_list = []
+        for iy, yint in enumerate(y_intervals):
+            for xint in x_intervals:
+                logger.info(f"Compute partial dataset {i}/{n_intervals}")
+                slice_kwargs = {
+                    yname: slice(yint.left, yint.right),
+                    xname: slice(xint.left, xint.right),
+                }
+                ds = dset.isel(slice_kwargs).load()
+                # Function needs to have self and dset as 1st and 2nd args
+                dsout = func(args[0], ds, *args[2:], **kwargs)
+                dsout_list.append(dsout)
+                i += 1
+        dsout = xr.combine_by_coords(dsout_list)
+        return dsout
+
+    return wrapped_func
 
 
 def expand_time_group(dset, time_group):
@@ -119,11 +213,10 @@ def uv_to_spddir(u, v, coming_from=False):
         direc (array): directions (degree).
 
     """
-    ang_rot = 180 if coming_from else 0
-    vetor = u + v * 1j
-    mag = da.absolute(vetor)
-    direc = xr.ufuncs.angle(vetor, deg=True) + ang_rot
-    direc = da.mod(90 - direc, 360)
+    to_nautical = 270 if coming_from else 90
+    mag = da.sqrt(u ** 2 + v ** 2)
+    direc = da.rad2deg(da.arctan2(v, u))
+    direc = (to_nautical - direc) % 360
     return mag, direc
 
 
