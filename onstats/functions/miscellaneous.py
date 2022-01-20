c9a25dccdd3fb5b31f2d91fa0912537dad3ffb98
Rafael Guedes <r.guedes@oceanum.science>
2022-01-21 12:58:57 +1300

    Add pcount function

---
+++ onstats/functions/miscellaneous.py
@@ -0,0 +1,16 @@
+"""Miscellaneous functions."""
+
+
+def pcount(self, dset, dim):
+    """Percentaga count function.
+
+    Args:
+        dset (xr.Dataset): Dataset to reduce.
+        dim (str): Dimension along which percentage count is calculated.
+
+    Returns:
+        dsout (xr.Dataset): Reduced dataset.
+
+    """
+    dsout = 100 * dset.count(dim) / dset[dim].size
+    return dsout
