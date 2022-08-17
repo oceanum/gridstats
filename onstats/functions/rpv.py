16bf8947de0e5ee5e31e8df20fc1fe100d801eed
Rafael Guedes <r.guedes@oceanum.science>
2022-08-17 17:48:35 -0300

    Try and fix recursive problem in rpv when running with dask

--- onstats/functions/rpv.py
+++ onstats/functions/rpv.py
@@ -39,7 +39,19 @@ def rpv(
         - rpvs (xr.Dataset): Return period values dataset.
 
     """
+    # Distribution function
+    try:
+        func = getattr(stats, distribution)
+    except AttributeError as err:
+        raise ValueError(
+            f"Distribution '{distribution}' not available in scipy.stats, valid "
+            f"distributions are: {[f for f in dir(stats) if f[0].islower()]}"
+        ) from err
+
+    # Data time resolution
     dt_hour = get_timestep(dset, dim).total_seconds() / 3600
+    if duration < dt_hour:
+        raise ValueError(f"dt {dt_hour}h must be less than storm duration {duration}h")
 
     # Grouping by
     if group is not None:
@@ -50,12 +62,12 @@ def rpv(
     dsout = xr.apply_ufunc(
         _np_rpv,
         dset,
+        func,
         dt_hour,
         return_periods,
         percentile,
-        distribution,
         duration,
-        input_core_dims=[[dim], [], ["period"], [], [], []],
+        input_core_dims=[[dim], [], [], ["period"], [], []],
         output_core_dims=[["period"]],
         exclude_dims=set((dim,)),
         vectorize=True,
@@ -83,7 +95,7 @@ def rpv(
     return dsout.transpose("period", ...).chunk({"period": 1})
 
 
-def _pov(data, dt_hour, percentile=95, duration=24):
+def _pot(data, dt_hour, percentile=95, duration=24):
     """Peaks over threshold.
 
     Args:
@@ -96,8 +108,6 @@ def _pov(data, dt_hour, percentile=95, duration=24):
         - height (float): Threshold value above which peaks are defined.
 
     """
-    if duration < dt_hour:
-        raise ValueError(f"dt {dt_hour}h must be less than storm duration {duration}h")
     distance = duration / dt_hour
     ind_perc = int(0.01 * percentile * data.shape[0])
     height = np.sort(data)[ind_perc]
@@ -107,52 +117,38 @@ def _pov(data, dt_hour, percentile=95, duration=24):
 
 def _np_rpv(
     data,
+    func,
     dt_hour,
     return_periods=[1, 5, 10, 20, 50, 100, 1000, 10000],
     percentile=95,
-    distribution="gumbel_r",
     duration=24,
 ):
     """Return period values from numpy array.
 
     Args:
         - data (1d array): Data to calculate rpv for.
+        - func (obj): Statistical distribution for fitting the data, any valid
+          distribution function in scipy.stats, e.g., `scipy.stats.gumbel_r`.
         - dt_hour(float): Time step in data.
         - return_periods (list): Return period years to calculate rpv values for.
         - percentile (float): Percentile above which peaks are selected.
-        - distribution (str): Statistical distribution to fit the data, any valid
-          distribution in scipy.stats, e.g., "gumbel_r", "weibull_min", etc.
         - duration (float): Hours in storm below which extra peaks are discarded.
 
     Returns:
         - rpvs (1d array): Return period values for years in return_periods.
 
     """
-    # Ensure valid distrubution
-    try:
-        func = getattr(stats, distribution)
-    except AttributeError as err:
-        raise ValueError(
-            f"Distribution '{distribution}' not available in scipy.stats, valid "
-            f"distributions are: {[f for f in dir(stats) if f[0].islower()]}"
-        ) from err
-
-    peaks, height = _pov(data, dt_hour, percentile, duration)
-
-    if peaks.size == 0:
-        logger.debug(
-            f"No peaks over {height} ({percentile}th percentile), returning nan"
-        )
-        return da.from_array([np.nan] * len(return_periods), chunks=(1,))
-
-    fits = func.fit(peaks, floc=height)
-    dt_year = dt_hour / (24 * 365)
-    ntimes = data.shape[0]
-    npeaks = peaks.shape[0]
-    rpvs = []
-    for return_period in return_periods:
-        p = ntimes * dt_year / (return_period * npeaks)
-        rpvs.append(func.isf(p, *fits))
+    peaks, height = _pot(data, dt_hour, percentile, duration)
+    rpvs = [np.nan] * len(return_periods)
+    if peaks.size > 0:
+        fits = func.fit(peaks, floc=height)
+        dt_year = dt_hour / (24 * 365)
+        ntimes = data.shape[0]
+        npeaks = peaks.shape[0]
+        rpvs = []
+        for return_period in return_periods:
+            p = ntimes * dt_year / (return_period * npeaks)
+            rpvs.append(func.isf(p, *fits))
     return da.from_array(rpvs, chunks=(1,))
 
 
