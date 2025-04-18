1a22513fe5a2fb96117efc9fe79a84b59170bc77
Rafael Guedes <r.guedes@oceanum.science>
2025-04-18 16:41:15 +1200

    Replace intake-oceanum install by intake-forecast

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ requirements = [
     "dask",
     "distributed",
     "flox",
+    "intake-forecast",
     "matplotlib",
     "nco",
     "pyyaml",
@@ -24,7 +25,7 @@ requirements = [
     "tqdm",
     "zarr",
     "oncore @ git+https://oceanum-dev:REDACTED@gitlab.com/oceanum/oncore@v0.1.28",
-    "intake-oceanum @ git+https://oceanum-dev:REDACTED@gitlab.com/oceanum/intake/intake-oceanum@copernicus-marine",
+    # "intake-oceanum @ git+https://oceanum-dev:REDACTED@gitlab.com/oceanum/intake/intake-oceanum@copernicus-marine",
     # "onstats @ git+https://oceanum-dev:REDACTED@gitlab.com/oceanum/onstats@master",
 ]
 
